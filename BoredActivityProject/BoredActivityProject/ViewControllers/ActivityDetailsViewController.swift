3387e38004bf8dbcdbd908c29e0fe8751150d0c1
luciana <lucianat.petre@gmail.com>
2021-11-19 22:46:46 +0100

    Add playing sound implementation

--- BoredActivityProject/BoredActivityProject/ViewControllers/ActivityDetailsViewController.swift
+++ BoredActivityProject/BoredActivityProject/ViewControllers/ActivityDetailsViewController.swift
@@ -5,8 +5,10 @@
 //  Created by Luciana Raducu on 19.11.21.
 //
 
-import Foundation
+import AVFoundation
 import UIKit
+
+var player: AVAudioPlayer?
 
 class ActivityDetailsViewController: UIViewController {
     @IBOutlet var activityName: UILabel!
@@ -17,15 +19,36 @@ class ActivityDetailsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupTexts()
         setupUI()
+        playSound()
     }
 }
 
 private extension ActivityDetailsViewController {
-    func setupUI() {
+    func setupTexts() {
         activityName.text = viewModel?.activityName
         price.text = viewModel?.price
         numberOfPArticipants.text = viewModel?.numberOfParticipants
+    }
+    
+    func setupUI() {
+        view.backgroundColor = viewModel?.activityColor
+    }
+    
+    func playSound() {
+        do {
+            guard let url = viewModel?.soundResource else { return }
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+            
+            player?.play()
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
     }
 }
 
