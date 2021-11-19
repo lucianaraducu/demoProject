e84b5fa7754fc7c375502ac90c5cf046718545f4
luciana <lucianat.petre@gmail.com>
2021-11-19 16:20:18 +0100

    Initial project setup

---
+++ BoredActivityProject/BoredActivityProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BoredActivityProject
+//
+//  Created by Luciana Raducu on 19.11.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
